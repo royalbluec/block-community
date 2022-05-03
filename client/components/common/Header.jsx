a1b622d1d580a96980d0dbebaa0487d2147d79e8
royalbluee <tkyeom96@gmail.com>
2022-05-03 11:42:33 +0900

    feat(client): Add Header component

---
+++ client/components/common/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <div>Logo</div>
+      <div>Input</div>
+      <div>SignIn</div>
+    </header>
+  );
+};
+
+export default Header;
