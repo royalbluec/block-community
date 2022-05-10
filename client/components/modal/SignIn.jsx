71df54a9e37567308b57bd88db415b4807704360
royalbluee <tkyeom96@gmail.com>
2022-05-10 22:51:23 +0900

    feat(client): Add handler in SignIn and SignUp

--- client/components/modal/SignIn.jsx
+++ client/components/modal/SignIn.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 
 import {
   StyledSignInHeader,
@@ -8,15 +8,35 @@ import {
 } from './styles/SignIn.styles';
 
 const SignIn = ({ clickIsSignIn }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const changeEmail = useCallback((e) => {
+    setEmail(e.target.value);
+  }, []);
+
+  const changePassword = useCallback((e) => {
+    setPassword(e.target.value);
+  }, []);
+
+  console.log('signin email', email);
+  console.log('signin password', password);
+
   return (
     <div>
       <StyledSignInHeader>Sign In</StyledSignInHeader>
       <div>
         <div>
-          <StyledSignInInput placeholder='email' />
+          <StyledSignInInput
+            placeholder='email'
+            onChange={(e) => changeEmail(e)}
+          />
         </div>
         <div>
-          <StyledSignInInput placeholder='password' />
+          <StyledSignInInput
+            placeholder='password'
+            onChange={(e) => changePassword(e)}
+          />
         </div>
         <StyledSignInButton>Sign In</StyledSignInButton>
       </div>
--- client/components/modal/Signup.jsx
+++ client/components/modal/Signup.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 
 import {
   StyledSignUpHeader,
@@ -8,18 +8,47 @@ import {
 } from './styles/SignUp.styles';
 
 const Signup = ({ clickIsSignIn }) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const changeName = useCallback((e) => {
+    setName(e.target.value);
+  }, []);
+
+  const changeEmail = useCallback((e) => {
+    setEmail(e.target.value);
+  }, []);
+
+  const changePassword = useCallback((e) => {
+    setPassword(e.target.value);
+  }, []);
+
+  console.log('signup name', name);
+  console.log('signup email', email);
+  console.log('signup password', password);
+
   return (
     <div>
       <StyledSignUpHeader>Sign Up</StyledSignUpHeader>
       <div>
         <div>
-          <StyledSignUpInput placeholder='name' />
+          <StyledSignUpInput
+            placeholder='name'
+            onChange={(e) => changeName(e)}
+          />
         </div>
         <div>
-          <StyledSignUpInput placeholder='email' />
+          <StyledSignUpInput
+            placeholder='email'
+            onChange={(e) => changeEmail(e)}
+          />
         </div>
         <div>
-          <StyledSignUpInput placeholder='password' />
+          <StyledSignUpInput
+            placeholder='password'
+            onChange={(e) => changePassword(e)}
+          />
         </div>
         <StyledSignUpButton>Sign Up</StyledSignUpButton>
       </div>
