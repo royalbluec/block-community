156ac43f88e1e7d881a98e1f002afc560e9e9ddb
royalbluee <tkyeom96@gmail.com>
2022-05-14 01:47:44 +0900

    feat(server): Add userController logic

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,13 +1,53 @@
+const User = require('../models/userSchema');
+
 const userController = {
-  get: (req, res) => {
-    res
-      .status(200)
-      .json({ success: true, message: '모든 유저를 가져왔습니다.' });
+  get: async (req, res) => {
+    const rawUserList = await User.find();
+
+    const userList = rawUserList.map((el) => {
+      return { id: el._id.toString(), email: el.email, name: el.name };
+    });
+
+    res.status(200).json({
+      success: true,
+      data: userList,
+      message: '모든 유저를 성공적으로 가져왔습니다.',
+    });
   },
-  post: (req, res) => {
-    res
-      .status(200)
-      .json({ success: true, message: '새로운 유저를 생성했습니다.' });
+  post: async (req, res) => {
+    const { email, password, name } = req.body;
+
+    if (!email || !password || !name) {
+      res.status(400).json({
+        success: false,
+        data: null,
+        message: '회원정보를 입력하지 않았습니다.',
+      });
+    } else {
+      const isEmailDuplicated = await User.findOne({ email: email });
+      const isNameDuplicated = await User.findOne({ name: name });
+
+      if (isEmailDuplicated) {
+        return res.status(400).json({
+          success: false,
+          data: null,
+          message: '이미 사용중인 이메일입니다.',
+        });
+      } else if (isNameDuplicated) {
+        return res.status(400).json({
+          success: false,
+          data: null,
+          message: '이미 사용중인 이름입니다.',
+        });
+      } else {
+        await User.create({ email, password, name });
+        return res.status(400).json({
+          success: true,
+          data: null,
+          message: '회원가입이 성공적으로 완료했습니다.',
+        });
+      }
+    }
   },
 };
 
--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema(
   {
     email: { type: String, required: true },
-    pw: { type: String, required: true },
+    password: { type: String, required: true },
     name: { type: String, required: true },
   },
   { timestamps: true },
