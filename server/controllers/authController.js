40fdd8150841bba1376116724aebfacf38106a37
royalbluee <tkyeom96@gmail.com>
2022-05-14 16:18:05 +0900

    feat(server): Add authController logic

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,9 +1,30 @@
+const User = require('../models/userSchema');
+const jwt = require('jsonwebtoken');
+
+const ACCESS_SECRET = process.env.ACCESS_SECRET || 'access';
+
 const authController = {
   post: async (req, res) => {
-    return res.status(200).json({
-      success: true,
-      data: null,
-      message: '사용자 인증을 성공했습니다.',
+    const { email, password } = req.body;
+
+    await User.findOne({ email, password }).then((userInfo) => {
+      if (!userInfo) {
+        return res.status(400).json({
+          success: false,
+          data: null,
+          message: '이메일과 암호가 일치하지 않습니다.',
+        });
+      } else {
+        const { email, name } = userInfo;
+        const payload = { email, name };
+        const accessToken = jwt.sign(payload, ACCESS_SECRET);
+
+        return res.status(200).json({
+          success: true,
+          data: accessToken,
+          message: '인증을 성공적으로 완료했습니다.',
+        });
+      }
     });
   },
 };
