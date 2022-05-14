10096de3ad25f3dab57810f4fe4953f5b96dcdcf
royalbluee <tkyeom96@gmail.com>
2022-05-14 14:11:45 +0900

    feat(server): Add userDetailController logic

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -11,7 +11,7 @@ const userController = {
     res.status(200).json({
       success: true,
       data: userList,
-      message: '모든 유저를 성공적으로 가져왔습니다.',
+      message: '모든 유저를 가져오는데 성공했습니다.',
     });
   },
   post: async (req, res) => {
@@ -24,6 +24,8 @@ const userController = {
         message: '회원정보를 입력하지 않았습니다.',
       });
     } else {
+      // 중복 이메일 체크
+      // 중복 이름 체크
       const isEmailDuplicated = await User.findOne({ email: email });
       const isNameDuplicated = await User.findOne({ name: name });
 
@@ -44,7 +46,7 @@ const userController = {
         return res.status(400).json({
           success: true,
           data: null,
-          message: '회원가입이 성공적으로 완료했습니다.',
+          message: '회원가입을 성공했습니다.',
         });
       }
     }
@@ -52,17 +54,91 @@ const userController = {
 };
 
 const userDetailController = {
-  get: (req, res) => {
-    console.log(req.params.id);
-    res.status(200).json({ success: true, message: '유저를 가져왔습니다' });
+  get: async (req, res) => {
+    const id = req.params.id;
+
+    try {
+      const { _id, email, name } = await User.findOne({ _id: id });
+      const userData = { id: _id.toString(), email, name };
+
+      return res.status(200).json({
+        success: true,
+        data: userData,
+        message: '유저 데이터를 가져오는데 성공했습니다.',
+      });
+    } catch (e) {
+      return res.status(400).json({
+        success: false,
+        data: null,
+        message: '유저 데이터를 가져오는데 실패했습니다.',
+      });
+    }
   },
-  put: (req, res) => {
-    console.log(req.params.id);
-    res.status(200).json({ success: true, message: '유저를 수정했습니다' });
+  put: async (req, res) => {
+    const id = req.params.id;
+    const { name, password } = req.body;
+    console.log(name, password);
+
+    try {
+      if (name && !password) {
+        // 중복 이름 체크
+        const isNameDuplicated = await User.findOne({ name: name });
+
+        if (isNameDuplicated) {
+          await User.findByIdAndUpdate(id, { name });
+          return res.status(200).json({
+            success: false,
+            data: null,
+            message: '이미 사용중인 이름입니다.',
+          });
+        } else {
+          await User.findByIdAndUpdate(id, { name });
+          return res.status(200).json({
+            success: true,
+            data: null,
+            message: '유저 이름을 수정하는데 성공했습니다.',
+          });
+        }
+      } else if (!name && password) {
+        await User.findByIdAndUpdate(id, { password });
+        return res.status(200).json({
+          success: true,
+          data: null,
+          message: '유저 비밀번호를 수정하는데 성공했습니다.',
+        });
+      } else {
+        return res.status(400).json({
+          success: false,
+          data: null,
+          message: '잘못된 값을 입력했습니다.',
+        });
+      }
+    } catch (e) {
+      return res.status(400).json({
+        success: false,
+        data: null,
+        message: '유저 데이터를 수정하는데 실패했습니다.',
+      });
+    }
   },
-  delete: (req, res) => {
-    console.log(req.params.id);
-    res.status(200).json({ success: true, message: '유저를 삭제했습니다.' });
+  delete: async (req, res) => {
+    const id = req.params.id;
+
+    try {
+      await User.findByIdAndDelete(id);
+
+      return res.status(200).json({
+        success: true,
+        data: null,
+        message: '유저 데이터를 삭제하는데 성공했습니다.',
+      });
+    } catch (e) {
+      return res.status(400).json({
+        success: false,
+        data: null,
+        message: '유저 데이터를 삭제하는데 실패했습니다.',
+      });
+    }
   },
 };
 
