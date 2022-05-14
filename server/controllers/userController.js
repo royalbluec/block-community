1c0592f85853260e27391f5323513fdcb7ab1c35
royalbluee <tkyeom96@gmail.com>
2022-05-15 02:48:30 +0900

    feat(server): Modify userController logic

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,53 +2,72 @@ const User = require('../models/userSchema');
 
 const userController = {
   get: async (req, res) => {
-    const rawUserList = await User.find();
+    try {
+      await User.find().then((userInfoList) => {
+        const userList = userInfoList.map((el) => {
+          return { id: el._id.toString(), email: el.email, name: el.name };
+        });
 
-    const userList = rawUserList.map((el) => {
-      return { id: el._id.toString(), email: el.email, name: el.name };
-    });
-
-    res.status(200).json({
-      success: true,
-      data: userList,
-      message: '모든 유저를 가져오는데 성공했습니다.',
-    });
+        res.status(200).json({
+          success: true,
+          data: userList,
+          message: '모든 유저를 가져오는데 성공했습니다.',
+        });
+      });
+    } catch (e) {
+      res.status(400).json({
+        success: false,
+        data: null,
+        message: '모든 유저를 가져오는데 실패했습니다.',
+      });
+    }
   },
   post: async (req, res) => {
     const { email, password, name } = req.body;
 
+    // 유효한 회원정보인지 검증
     if (!email || !password || !name) {
       res.status(400).json({
         success: false,
         data: null,
-        message: '회원정보를 입력하지 않았습니다.',
+        message: '회원정보를 잘못 입력했습니다.',
       });
-    } else {
-      // 중복 이메일 체크
-      // 중복 이름 체크
-      const isEmailDuplicated = await User.findOne({ email: email });
-      const isNameDuplicated = await User.findOne({ name: name });
+    }
 
-      if (isEmailDuplicated) {
+    try {
+      // 중복 이메일인지 검증
+      const duplicatedEmail = await User.findOne({ email: email });
+      if (duplicatedEmail) {
         return res.status(400).json({
           success: false,
           data: null,
           message: '이미 사용중인 이메일입니다.',
         });
-      } else if (isNameDuplicated) {
+      }
+
+      // 중복 이름인지 검증
+      const duplicatedName = await User.findOne({ name: name });
+      if (duplicatedName) {
         return res.status(400).json({
           success: false,
           data: null,
           message: '이미 사용중인 이름입니다.',
         });
-      } else {
-        await User.create({ email, password, name });
-        return res.status(400).json({
+      }
+
+      await User.create({ email, password, name }).then(() => {
+        return res.status(200).json({
           success: true,
           data: null,
           message: '회원가입을 성공했습니다.',
         });
-      }
+      });
+    } catch (e) {
+      return res.status(400).json({
+        success: true,
+        data: null,
+        message: '회원가입을 실패했습니다.',
+      });
     }
   },
 };
@@ -90,6 +109,15 @@ const userDetailController = {
     const id = req.params.id;
     const { password } = req.body;
 
+    // 유효한 비밀번호인지 검증
+    if (!password) {
+      return res.status(400).json({
+        success: false,
+        data: null,
+        message: '새로운 비밀번호를 잘못 입력했습니다.',
+      });
+    }
+
     try {
       if (id !== tokenID) {
         return res.status(403).json({
@@ -98,22 +126,13 @@ const userDetailController = {
           message: '유저 데이터를 수정하는 권한이 없습니다.',
         });
       } else {
-        // password 규칙
-        if (!password) {
-          return res.status(400).json({
-            success: false,
+        await User.findByIdAndUpdate(id, { password }).then(() => {
+          return res.status(200).json({
+            success: true,
             data: null,
-            message: '새로운 비밀번호를 잘못 입력했습니다.',
+            message: '유저 데이터를 수정하는데 성공했습니다.',
           });
-        } else {
-          await User.findByIdAndUpdate(id, { password }).then(() => {
-            return res.status(200).json({
-              success: true,
-              data: null,
-              message: '유저 데이터를 수정하는데 성공했습니다.',
-            });
-          });
-        }
+        });
       }
     } catch (e) {
       return res.status(400).json({
